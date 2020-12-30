58ce50d1c74b9b872f7d07afe330b4ac97f50975
GIGA-Money <christopherregan@outlook.com>
2020-12-29 19:51:35 -0500

    daily update, learning numpy array transpose

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def current_output(inputs, weights, biases):
     return layer_outputs
 
 def quick_dot_prod(inputs, weights, bias):
-    output = np.dot(weights, inputs) + bias)
+    output = np.dot(inputs, np.array(weights).T) + bias
 	return output
 
 def main(argv):
@@ -26,11 +26,14 @@ def main(argv):
         raise app.UsageError("Expected one command-line argument(s), "
                              f"got: {argv}")
 
-    inputs = [1, 2, 3, 2.5]
+    inputs = [[1, 2, 3, 2.5],
+				[2.0, 5.0, -1.0, 2.0],
+				[-1.5, 2.7, 3.3, -0.8]]
 
     weights = [[0.2, 0.8, -0.5, 1.0],
                [0.5, -0.91, 0.26, -0.5],
                [-0.26, -0.27, .17, 0.87]]
+			   
     biases = [2, 3, 0.5]
 
 	output = quick_dot_prod(inputs, weights, biases)
