63fa0c146469a5a5a3c8def7a698632596494773
GIGA-Money <gigachris1@hotmail.com>
2021-01-19 16:11:25 -0500

    corrected layer dense initialization, correcet some naming conventions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
+from abc import ABCMeta, abstractmethod
 import os
 import numpy as np
-import matplotlib
 from absl import app
 from absl import flags
 
@@ -28,23 +27,41 @@ def quick_dot_prod(inputs, weights, bias):
     return output
 
 
-class Layer_Dense:
-    def __inti__(self, n_input, n_neurons):
+class LayerDense:
+    def __init__(self, n_input, n_neurons):
+        '''
+        :param n_input:
+        :param n_neurons:
+        :return:
+        '''
         self.weights = 0.1 * np.random.randn(n_input, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
-        pass
+        '''
+        :param inputs:
+        :return:
+        '''
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 
 def main(argv):
     if len(argv) > 2:
         raise app.UsageError("Expected one command-line argument(s), "
                              f"got: {argv}")
+
     np.random.seed(FLAGS.Random_seed)
+
     X = [[1, 2, 3, 2.5],
          [2.0, 5.0, -1.0, 2.0],
          [-1.5, 2.7, 3.3, -0.8]]
+
+    layer1 = LayerDense(2, 5)
+    layer2 = LayerDense(5, 4)
+
+    layer1.forward(X)
+    layer2.forward(layer1.output)
+    print(layer2.output)
 
     os._exit(0)
 
