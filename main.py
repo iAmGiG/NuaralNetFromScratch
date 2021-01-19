87f3b4678de95c9c30ed667fb1944a3e47122952
GIGA-Money <gigachris1@hotmail.com>
2021-01-19 15:37:44 -0500

    updated flag to use integer vs. float, as random seed cannot be a float

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import matplotlib
 from absl import app
 from absl import flags
 
-flags.DEFINE_float("Random_seed", 0.0, "The seed given to np.random.seed, default is 0.0")
+flags.DEFINE_integer("Random_seed", 0, "The seed given to np.random.seed, default is 0")
 
 FLAGS = flags.FLAGS
+
 
 def current_output(inputs, weights, biases):
     layer_outputs = []
@@ -21,32 +22,32 @@ def current_output(inputs, weights, biases):
 
     return layer_outputs
 
+
 def quick_dot_prod(inputs, weights, bias):
     output = np.dot(inputs, np.array(weights).T) + bias
-	return output
+    return output
+
 
 class Layer_Dense:
-	def __inti__(self, n_input, n_neurons):
-		self.weights = 0.1 * np.random.randn(n_input, n_neurons)
-		self.biases = np.zeros((1, n_neurons))
-	def forward(self, inputs):
-		pass
-		
+    def __inti__(self, n_input, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_input, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        pass
+
 
 def main(argv):
     if len(argv) > 2:
         raise app.UsageError("Expected one command-line argument(s), "
                              f"got: {argv}")
-
+    np.random.seed(FLAGS.Random_seed)
     X = [[1, 2, 3, 2.5],
-				[2.0, 5.0, -1.0, 2.0],
-				[-1.5, 2.7, 3.3, -0.8]]
-
-    
+         [2.0, 5.0, -1.0, 2.0],
+         [-1.5, 2.7, 3.3, -0.8]]
 
     os._exit(0)
 
 
 if __name__ == '__main__':
-	np.random.seed(FLAGS.Random_seed)
     app.run(main)
